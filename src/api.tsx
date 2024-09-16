8f4e357aeb3c634e814f8344180d7643bd8e3ca4
jennifer hodge <jennifer.hodge@russdarrow.com>
2024-09-15 19:34:15 -0500

    first commit

--- src/api.tsx
+++ src/api.tsx
@@ -1,19 +1,69 @@
+import { Dog } from "./types.ts";
+
+
 export const baseUrl = "http://localhost:3000";
 
-export const Requests = {
+
+const myHeaders = {
+	"Content-Type": "application/json",
+};
+
+export const Requests = { 
+   
+
+
   // should return a promise with all dogs in the database
-  getAllDogs: () => {},
+  getAllDogs: async () => {
+		return fetch("http://localhost:3000", {
+			method: "GET",
+			headers: myHeaders,
+
+		}).then((response) => response.json());
+		
+	},
+
+
+
+ 
   // should create a dog in the database from a partial dog object
   // and return a promise with the result
-  postDog: () => {},
+  postDog: async ({ name, image, description, isFavorite }: Omit<Dog, "id">) => {
+		return fetch(baseUrl + "/dogs", {
+			method: "POST",
+			headers: myHeaders,
+			body: JSON.stringify({
+				name: name,
+				image: image,
+				description: description,
+				isFavorite: isFavorite,
+			}),
+		}).then((response) => response.json());
+	},
 
   // should delete a dog from the database
-  deleteDog: () => {},
+  deleteDog: async (idNum: number) => {
+		return fetch(baseUrl + `/dogs/${idNum}`, {
+			method: "DELETE",
+			headers: myHeaders,
+		}).then((response) => response.json());
+	},  
 
-  updateDog: () => {},
+
+  updateDog: async(idNum: number, isFavorite: boolean) => {
+    return fetch(`${baseUrl}/dogs/${idNum}`, {
+      method: "PAtCH",
+      headers: myHeaders,
+        body: JSON.stringify({
+          isFavorite: !isFavorite,
+        })
+      }).then((response) => response.json());
+    
+    },
+  
 
   // Just a dummy function for use in the playground
   dummyFunction: () => {
     console.log("dummy stuff");
   },
 };
+
--- src/types.ts
+++ src/types.ts
@@ -1,1 +1,8 @@
 // Add your own custom types in here
+export type Dog = {
+	id: number,
+	name: string,
+	image: string,
+	description: string,
+	isFavorite: boolean,
+};
